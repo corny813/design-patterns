4d50ae4dc75e051db3d9f42b1f59dc9678eed4f7
corny <corny@manfo-PC>
2015-06-19 16:05:08 +0800

    decorator/prototype

--- DesignPatterns/src/prototype/Prototype.java
+++ DesignPatterns/src/prototype/Prototype.java
@@ -88,13 +88,15 @@ public class Prototype implements Cloneable,Serializable{
 		SerializableObject obj = c1.getObject();
 		int num = c1.getNum();
 		String str = c1.getStr();
+		//基本类型、String类型、对象类型是否== （String为同一引用则==为true）
 		System.out.println((p.getNum()==num)+","+(p.getStr()==str)+","+(p.getObject()==obj));
 		
 		Prototype c2 = (Prototype) p.deepClone();
 		num = c2.getNum();
 		str = c2.getStr();
 		obj = c2.getObject();
-		System.out.println((p.getNum()==num)+","+(p.getStr()==str)+","+(p.getObject()==obj));
+		//基本类型、String类型、对象类型是否== （String为两份引用则==为false）
+		System.out.println((p.getNum()==num)+","+(p.getStr()==str)+","+(p.getStr().equals(str))+","+(p.getObject()==obj));
 
 		System.out.println((p==c1)+","+(p==c2)+";"+(p.equals(c1)+","+(p.equals(c2))));
 	}
